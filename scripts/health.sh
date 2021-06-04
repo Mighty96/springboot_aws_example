de815a7fd4fb5fd9e1ac358cba3ebdebe16a9bbc
Mighty96 <mightyspade96@gmail.com>
2021-06-04 20:11:12 +0900

    [21.06.04] Modify a script

--- scripts/health.sh
+++ scripts/health.sh
@@ -2,7 +2,9 @@
 
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
-source ${ABSDIR}/profile.bashsource ${ABSDIR}/switch.bash
+source ${ABSDIR}/profile.sh
+source ${ABSDIR}/switch.sh
+
 IDLE_PORT=$(find_idle_port)
 
 echo "> Health Check Start!"
